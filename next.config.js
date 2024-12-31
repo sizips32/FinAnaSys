96f98bf4d3f7602879187e671ab36fa1d59c09ed
sizips32 <58421243+sizips32@users.noreply.github.com>
2025-01-01 06:42:21 +0900

    third updates

---
+++ next.config.js
@@ -0,0 +1,10 @@
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  swcMinify: true,
+  reactStrictMode: true,
+  poweredByHeader: false
+}
+
+module.exports = nextConfig
